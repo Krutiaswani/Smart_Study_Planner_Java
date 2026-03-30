513710ad1a6ed31dfb4ee9b9942829a4fa542b52
Kruti Aswani <krutiaswani8440@gmail.com>
2026-03-30 17:34:52 +0530

    Final Version with GUI

--- Main.java
+++ Main.java
@@ -1,3 +1,6 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 import java.util.*;
 
 class Subject {
@@ -5,62 +8,81 @@ class Subject {
     int difficulty;
     int deadlineDays;
 
-    Subject(String name, int difficulty, int deadlineDays) {
+    public Subject(String name, int difficulty, int deadlineDays) {
         this.name = name;
         this.difficulty = difficulty;
         this.deadlineDays = deadlineDays;
     }
 }
 
-class StudyPlanner {
-    public static Map<String, Integer> generatePlan(List<Subject> subjects, int hours) {
-        Map<String, Integer> plan = new HashMap<>();
-
-        int totalWeight = 0;
-        Map<String, Integer> weights = new HashMap<>();
-
-        for (Subject s : subjects) {
-            int weight = s.difficulty * (10 - s.deadlineDays);
-            if (weight < 1) weight = 1;
-            weights.put(s.name, weight);
-            totalWeight += weight;
-        }
-
-        for (Subject s : subjects) {
-            int time = (weights.get(s.name) * hours) / totalWeight;
-            plan.put(s.name, time);
-        }
-
-        return plan;
-    }
-}
-
 public class Main {
+    static ArrayList<Subject> subjects = new ArrayList<>();
+
     public static void main(String[] args) {
 
-        List<Subject> subjects = new ArrayList<>();
+        JFrame frame = new JFrame("Smart Study Planner");
+        frame.setSize(500, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new FlowLayout());
 
-        subjects.add(new Subject("Math", 5, 2));
-        subjects.add(new Subject("Physics", 4, 5));
-        subjects.add(new Subject("English", 2, 7));
+        JTextField nameField = new JTextField(10);
+        JTextField difficultyField = new JTextField(5);
+        JTextField deadlineField = new JTextField(5);
 
-        int studyHours = 6;
+        JButton addButton = new JButton("Add Subject");
+        JTextArea outputArea = new JTextArea(15, 40);
 
-        Map<String, Integer> plan = StudyPlanner.generatePlan(subjects, studyHours);
+        frame.add(new JLabel("Subject:"));
+        frame.add(nameField);
 
-        System.out.println("Study Plan:");
-        for (String subject : plan.keySet()) {
-            System.out.println(subject + ": " + plan.get(subject) + " hours/day");
-        }
-        System.out.println("\nAI Suggestions:");
+        frame.add(new JLabel("Difficulty (1-5):"));
+        frame.add(difficultyField);
 
-        for (Subject s : subjects) {
-            if (s.difficulty >= 4) {
-                System.out.println("Focus more on " + s.name + " (High Difficulty)");
+        frame.add(new JLabel("Deadline (days):"));
+        frame.add(deadlineField);
+
+        frame.add(addButton);
+        frame.add(new JScrollPane(outputArea));
+
+        addButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String name = nameField.getText();
+                int difficulty = Integer.parseInt(difficultyField.getText());
+                int deadline = Integer.parseInt(deadlineField.getText());
+
+                subjects.add(new Subject(name, difficulty, deadline));
+
+                outputArea.setText("");
+
+                outputArea.append("Study Plan:\n");
+
+                int totalDifficulty = 0;
+                for (Subject s : subjects) {
+                    totalDifficulty += s.difficulty;
+                }
+
+                for (Subject s : subjects) {
+                    int hours = (s.difficulty * 6) / totalDifficulty;
+                    outputArea.append(s.name + ": " + hours + " hrs/day\n");
+                }
+
+                outputArea.append("\nAI Suggestions:\n");
+
+                for (Subject s : subjects) {
+                    if (s.difficulty >= 4) {
+                        outputArea.append("Focus more on " + s.name + "\n");
+                    }
+                    if (s.deadlineDays <= 3) {
+                        outputArea.append("Urgent: " + s.name + " deadline near!\n");
+                    }
+                }
+
+                nameField.setText("");
+                difficultyField.setText("");
+                deadlineField.setText("");
             }
-            if (s.deadlineDays <= 3) {
-                System.out.println("Urgent: " + s.name + " deadline is near!");
-            }   
-        }
+        });
+
+        frame.setVisible(true);
     }
 }
